06950c55f41865642af1f3b7ad0cf32bd1967294
jgraham <jgraham@4miles.com>
2021-12-06 14:38:58 -0500

    Day 6 Part 2

--- Day_6/lantern-fish.ts
+++ Day_6/lantern-fish.ts
@@ -1,12 +1,29 @@
 import FileParser from '../libraries/parse-data';
 
 export default class LanternFish {
-    static fishData: number[];
+    static fishData: { [key: number]: number } = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0 };
     static fishToAdd = 0;
 
+    static reinitializeFishData(): void {
+        LanternFish.fishData = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0 };
+    }
+
     static getFishData(initialState: string = ''): void {
-        LanternFish.fishData =
-            initialState === '' ? FileParser.readFile('./Day_6/model.txt').split(',').map(Number) : initialState.split(',').map(Number);
+        this.reinitializeFishData();
+        initialState === ''
+            ? FileParser.readFile('./Day_6/model.txt')
+                  .split(',')
+                  .map(Number)
+                  .map((spawnDay) => {
+                      if (spawnDay) {
+                          LanternFish.fishData[spawnDay]++;
+                      }
+                  })
+            : initialState.split(',').map((spawnDay) => {
+                  if (spawnDay) {
+                      LanternFish.fishData[spawnDay]++;
+                  }
+              });
     }
 
     static countFish(days: number, count = 1): void {
@@ -20,22 +37,30 @@ export default class LanternFish {
     }
 
     private static decrementCount() {
-        for (let i = 0; i < LanternFish.fishData.length; i++) {
-            const spawnDay = LanternFish.fishData[i];
-            if (spawnDay === 0) {
-                LanternFish.fishData.push(9);
-                LanternFish.fishData[i] = 6;
+        const newFish = LanternFish.fishData[0];
+        Object.keys(LanternFish.fishData).map((fishCount, index) => {
+            if (index === 8) {
+                LanternFish.fishData[index] = newFish;
+            } else if (index === 6) {
+                LanternFish.fishData[index] = LanternFish.fishData[index + 1] + newFish;
             } else {
-                LanternFish.fishData[i] = spawnDay - 1;
+                LanternFish.fishData[index] = LanternFish.fishData[index + 1];
             }
-        }
+        });
     }
 
     static outputSpawn(days: number): void {
         LanternFish.getFishData();
         LanternFish.countFish(days);
-        console.log(`After ${days} days, the fish population is ${LanternFish.fishData.length}`);
+        console.log(`After ${days} days, the fish population is ${this.sumFish(LanternFish.fishData)}`);
+    }
+
+    // function that loops over objects keys and sums the values
+    static sumFish(obj: { [key: number]: number }): number {
+        return Object.keys(obj).reduce((a, b) => {
+            return a + obj[b];
+        }, 0);
     }
 }
 
-LanternFish.outputSpawn(80);
+LanternFish.outputSpawn(256);
--- Day_6/tests/lantern-fish.spec.ts
+++ Day_6/tests/lantern-fish.spec.ts
@@ -10,31 +10,44 @@ import VentMapper from '../lantern-fish';
 describe('lantern-fish', () => {
     const initialState = '3,4,3,1,2';
 
-    it('should parse the initial state into an array', () => {
-        LanternFish.getFishData(initialState);
-        const result = LanternFish.fishData;
-        expect(result).toEqual([3, 4, 3, 1, 2]);
-    });
+    // it('should parse the initial state into an array', () => {
+    //     LanternFish.getFishData(initialState);
+    //     const result = LanternFish.fishData;
+    //     expect(result).toEqual([3, 4, 3, 1, 2]);
+    // });
 
     describe('count-fish', () => {
         beforeEach(() => {
             LanternFish.getFishData(initialState);
         });
-        it('should stop processing fish after 18 days', () => {
-            spyOn(LanternFish, 'countFish').and.callThrough();
-            LanternFish.countFish(18);
-            expect(LanternFish.countFish).toHaveBeenCalledTimes(18);
-        });
+        // it('should stop processing fish after 18 days', () => {
+        //     spyOn(LanternFish, 'countFish').and.callThrough();
+        //     LanternFish.countFish(18);
+        //     expect(LanternFish.countFish).toHaveBeenCalledTimes(18);
+        // });
 
         it('should decrement the fish count by 1 for each count', () => {
             LanternFish.countFish(1);
-            expect(LanternFish.fishData).toEqual([2, 3, 2, 0, 1]);
+            expect(LanternFish.fishData).toEqual({ 0: 1, 1: 1, 2: 2, 3: 1, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0 });
         });
     });
 
     it('should add a new fish at 8 count when it decrementing to negative', () => {
         LanternFish.getFishData(initialState);
         LanternFish.countFish(3);
-        expect(LanternFish.fishData).toEqual([0, 1, 0, 5, 6, 7, 8]);
+        expect(LanternFish.fishData).toEqual({ 0: 2, 1: 1, 2: 0, 3: 0, 4: 0, 5: 1, 6: 1, 7: 1, 8: 1 });
+    });
+
+    // create an object whose key is the current spawn day
+    // increment based on what is at the 0 key
+    it('should return a list of objects with keys for 0 thru 8', () => {
+        LanternFish.getFishData(initialState);
+        expect(LanternFish.fishData).toEqual({ 0: 0, 1: 1, 2: 1, 3: 2, 4: 1, 5: 0, 6: 0, 7: 0, 8: 0 });
+    });
+
+    it('should return a sum of all the fish', () => {
+        LanternFish.getFishData(initialState);
+        LanternFish.countFish(3);
+        expect(LanternFish.sumFish(LanternFish.fishData)).toEqual(7);
     });
 });
