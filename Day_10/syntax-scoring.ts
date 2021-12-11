ca635294e16a2ebe721d5cc0dfdd4e6a79a65f2e
John Graham <johngrhm@gmail.com>
2021-12-11 00:42:23 -0500

    10.2

--- Day_10/syntax-scoring.ts
+++ Day_10/syntax-scoring.ts
@@ -40,13 +40,19 @@ export default class SyntaxScoring {
         ['}', 1197],
         ['>', 25137],
     ]);
+    static autocompleteMap: Map<string, number> = new Map<string, number>([
+        [')', 1],
+        [']', 2],
+        ['}', 3],
+        ['>', 4],
+    ]);
     static parseChunks(): void {
         this.chunks = FileParser.readFile('./Day_10/model.txt').split('\n');
     }
 
     static findFirstIllegalChars(): string[] {
         let illegalChars: string[] = [];
-
+        const indicesToRemove: number[] = [];
         this.chunks.forEach((chunk: string, index) => {
             const stack = new Stack<string>();
 
@@ -61,12 +67,52 @@ export default class SyntaxScoring {
                         stack.pop();
                     } else {
                         illegalChars.push(char);
+                        indicesToRemove.push(index);
                         break;
                     }
                 }
             }
         });
+        this.chunks = this.chunks.filter((chunk, index) => {
+            return !indicesToRemove.includes(index);
+        });
         return illegalChars;
+    }
+
+    static findClosingChars(chunk: string): string {
+        const stack = new Stack<string>();
+        const chunkChars = chunk.split('');
+        const closingChars: string[] = [];
+        for (let i = 0; i < chunkChars.length; i++) {
+            const char = chunkChars[i];
+            if (char === '(' || char === '{' || char === '[' || char === '<') {
+                stack.push(char);
+            } else if (char === ')' || char === '}' || char === ']' || char === '>') {
+                if (this.isClosingChar(char, stack)) {
+                    stack.pop();
+                }
+            }
+        }
+        while (stack.size() > 0) {
+            closingChars.push(this.getClosingChar(stack.peek()));
+            stack.pop();
+        }
+        return closingChars.join('');
+    }
+
+    static getClosingChar(char: string): string {
+        switch (char) {
+            case '(':
+                return ')';
+            case '{':
+                return '}';
+            case '[':
+                return ']';
+            case '<':
+                return '>';
+            default:
+                return '';
+        }
     }
 
     static isClosingChar(char: string, stack: Stack<string>): boolean {
@@ -86,6 +132,34 @@ export default class SyntaxScoring {
         return isClosingChar;
     }
 
+    static getAutocompleteScores(): number[] {
+        const autocompleteScores: number[] = [];
+        this.findFirstIllegalChars();
+        this.chunks.forEach((chunk: string, index) => {
+            let score = 0;
+            this.findClosingChars(chunk)
+                .split('')
+                .forEach((char: string) => {
+                    if (this.autocompleteMap.has(char)) {
+                        score *= 5;
+                        score += this.autocompleteMap.get(char);
+                    }
+                });
+            autocompleteScores.push(score);
+        });
+        return autocompleteScores;
+    }
+
+    static outputAutocompleteScore(): void {
+        this.parseChunks();
+        const autocompleteScores: number[] = this.getAutocompleteScores().sort((a, b) => {
+            return b - a;
+        });
+
+        var middle = autocompleteScores[Math.round((autocompleteScores.length - 1) / 2)];
+        console.log(middle);
+    }
+
     static outputErrorScore(): void {
         this.parseChunks();
         const illegalChars = this.findFirstIllegalChars();
@@ -95,4 +169,4 @@ export default class SyntaxScoring {
         console.log(score);
     }
 }
-SyntaxScoring.outputErrorScore();
+// SyntaxScoring.outputAutocompleteScore();
--- Day_10/tests/syntax-scoring.spec.ts
+++ Day_10/tests/syntax-scoring.spec.ts
@@ -24,18 +24,23 @@ describe('syntax-scoring', () => {
             const result = SyntaxScoring.findFirstIllegalChars();
             expect(result).toEqual(['}', ')', ']', ')', '>']);
         });
-        // it('should filter out incomplete chunks', () => {
-        //     SyntaxScoring.filterIncompleteChunks();
-        //     expect(SyntaxScoring.chunks).toEqual([
-        //     '[({(<(())[]>[[{[]{<()<>>',
-        //     '{([(<{}[<>[]}>{[]{[(<()>',
-        // })
-
+        it('should filter out illegal chunks', () => {
+            const result = SyntaxScoring.findFirstIllegalChars();
+            expect(SyntaxScoring.chunks.length).toEqual(5);
+        });
         it('should return true when closing char matches last item on the stack', () => {
             const stack = new Stack<string>();
             stack.push('(');
             const result = SyntaxScoring.isClosingChar(')', stack);
             expect(result).toBe(true);
+        });
+        it('should return closing chars', () => {
+            const result = SyntaxScoring.findClosingChars('[({(<(())[]>[[{[]{<()<>>');
+            expect(result).toEqual('}}]])})]');
+        });
+        it('should return autocomplete score', () => {
+            const result = SyntaxScoring.getAutocompleteScores();
+            expect(result).toEqual([288957, 5566, 1480781, 995444, 294]);
         });
     });
 });
