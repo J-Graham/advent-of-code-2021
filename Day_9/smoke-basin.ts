4f2c00dd368309a8951cfb7680bd4c15eacb0a2a
John Graham <johngrhm@gmail.com>
2021-12-10 23:18:05 -0500

    9.2

--- Day_9/smoke-basin.ts
+++ Day_9/smoke-basin.ts
@@ -1,13 +1,19 @@
 import FileParser from '../libraries/parse-data';
 
 export default class SmokeBasin {
-    static smokeBasin: number[][] = [];
+    static smokeBasin: number[][];
+    static smallestCoords: number[][];
+    static basins: number[][];
+    static coordsToProcess: number[][] = [];
+    static basinCordsProcessed: number[][];
 
     static parseSmokeBasin(input: string = ''): void {
-        // SmokeBasin.smokeBasin =
+        SmokeBasin.smokeBasin = [];
+        this.basins = [];
+        this.smallestCoords = [];
         const parsedBasin = [];
         if (input === '') {
-            SmokeBasin.smokeBasin = FileParser.readFile('./Day_9/test-model.txt')
+            SmokeBasin.smokeBasin = FileParser.readFile('./Day_9/model.txt')
                 .split('\n')
                 .map((row) => {
                     parsedBasin.push(row.toString().trim().split('').map(Number));
@@ -19,4 +25,93 @@ export default class SmokeBasin {
         }
         this.smokeBasin = parsedBasin;
     }
+
+    static getSmallestAdjacent(x: number, y: number, saveCoords = false): number {
+        let smallest = SmokeBasin.smokeBasin[y][x];
+        const neighbors = [
+            y > 0 ? SmokeBasin.smokeBasin[y - 1][x] : Number.MAX_VALUE,
+            y < this.smokeBasin.length - 1 ? SmokeBasin.smokeBasin[y + 1][x] : Number.MAX_VALUE,
+            x > 0 ? SmokeBasin.smokeBasin[y][x - 1] : Number.MAX_VALUE,
+            x < this.smokeBasin[y].length - 1 ? SmokeBasin.smokeBasin[y][x + 1] : Number.MAX_VALUE,
+        ];
+        neighbors.forEach((neighbor, index) => {
+            if (neighbor !== 9 && neighbor !== Number.MAX_VALUE && saveCoords) {
+                // top, bottom, left, right
+                if (index === 0 && !this.coordWasProcessed([x, y - 1])) {
+                    this.coordsToProcess.push([x, y - 1]);
+                } else if (index === 1 && !this.coordWasProcessed([x, y + 1])) {
+                    this.coordsToProcess.push([x, y + 1]);
+                } else if (index === 2 && !this.coordWasProcessed([x - 1, y])) {
+                    this.coordsToProcess.push([x - 1, y]);
+                } else if (index === 3 && !this.coordWasProcessed([x + 1, y])) {
+                    this.coordsToProcess.push([x + 1, y]);
+                }
+            }
+            if (neighbor <= smallest) {
+                smallest = -1;
+            }
+        });
+        return smallest;
+    }
+
+    static coordWasProcessed(coord: number[]): boolean {
+        return (
+            JSON.stringify(this.basinCordsProcessed).includes(JSON.stringify(coord)) ||
+            JSON.stringify(this.coordsToProcess).includes(JSON.stringify(coord))
+        );
+    }
+
+    static getSmallestNumbers(): number[] {
+        const smallestNumbers = [];
+        for (let i = 0; i < SmokeBasin.smokeBasin.length; i++) {
+            for (let j = 0; j < SmokeBasin.smokeBasin[i].length; j++) {
+                const smallest = this.getSmallestAdjacent(j, i);
+                if (smallest >= 0) {
+                    this.smallestCoords.push([j, i]);
+                    smallestNumbers.push(smallest);
+                }
+            }
+        }
+        return smallestNumbers;
+    }
+
+    static crawlBasin(): void {
+        this.parseSmokeBasin();
+        const smallestNumbers = this.getSmallestNumbers();
+        this.smallestCoords.forEach((smallest, index) => {
+            const basin = [];
+            this.basinCordsProcessed = [];
+            this.coordsToProcess.push([smallest[0], smallest[1]]);
+            basin.push(this.smokeBasin[smallest[1]][smallest[0]]);
+            while (this.coordsToProcess.length > 0) {
+                this.getSmallestAdjacent(this.coordsToProcess[0][0], this.coordsToProcess[0][1], true);
+                this.basinCordsProcessed.push(this.coordsToProcess[0]);
+                this.coordsToProcess.shift();
+                if (this.coordsToProcess.length > 0) {
+                    basin.push(this.smokeBasin[this.coordsToProcess[0][1]][this.coordsToProcess[0][0]]);
+                }
+            }
+            this.basins.push(basin);
+        });
+
+        let output = 1;
+        for (let i = 0; i < 3; i++) {
+            const max = Math.max(...this.basins.map((el) => el.length));
+            output *= max;
+            this.basins.splice(
+                this.basins.findIndex((el) => el.length === max),
+                1,
+            );
+        }
+        console.log(output);
+    }
+
+    static outputRiskLevel(): void {
+        this.parseSmokeBasin();
+        const smallestNumbers = this.getSmallestNumbers();
+        const riskLevel = smallestNumbers.reduce((a, b) => a + b + 1, 0);
+        console.log('risk level', riskLevel);
+    }
 }
+
+SmokeBasin.crawlBasin();
--- Day_9/tests/smoke-basin.spec.ts
+++ Day_9/tests/smoke-basin.spec.ts
@@ -1,6 +1,11 @@
 import SmokeBasin from '../smoke-basin';
 
 describe('smoke-basin', () => {
+    const basinRaw = `2199943210
+    3987894921
+    9856789892
+    8767896789
+    9899965678`;
     const basin = [
         [2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
         [3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
@@ -9,7 +14,22 @@ describe('smoke-basin', () => {
         [9, 8, 9, 9, 9, 6, 5, 6, 7, 8],
     ];
     it('should return 2d array of input', () => {
-        SmokeBasin.parseSmokeBasin();
+        SmokeBasin.parseSmokeBasin(basinRaw);
         expect(SmokeBasin.smokeBasin).toEqual(basin);
+    });
+
+    describe('post parse', () => {
+        beforeEach(() => {
+            SmokeBasin.parseSmokeBasin(basinRaw);
+        });
+        it('should return number if it is the smallest number in the vicinity', () => {
+            const result = SmokeBasin.getSmallestAdjacent(1, 0);
+            expect(result).toEqual(1);
+        });
+
+        it('should get an array of all the smallest numbers', () => {
+            const result = SmokeBasin.getSmallestNumbers();
+            expect(result).toEqual([1, 0, 5, 5]);
+        });
     });
 });
